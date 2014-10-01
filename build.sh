872a5f32fce8a8ea983ee75be669b4c18a64ca8d
Frank Jung <frankhjung@linux.com>
2014-10-01 15:27:24 +1000

    simplify tests

--- build.sh
+++ build.sh
@@ -6,23 +6,17 @@
 # * update bundles
 # * invoke rake
 
-# prepare version of Ruby environment
+# prepare ruby version for this environment
 [[ -s "${HOME}/.rvm/scripts/rvm" ]] && source "${HOME}/.rvm/scripts/rvm"
 [[ -s /usr/local/rvm/scripts/rvm ]] && source /usr/local/rvm/scripts/rvm
-if [[ -f .ruby-version && -f .ruby-gemset ]]; then 
-    RVM_GEMSET=$(cat .ruby-version .ruby-gemset | tr "\\n" "@" | sed 's/@$//')
-fi
-if [[ -z "${RVM_GEMSET}" ]]; then
-    echo "ERROR: no valid ruby gemset specified: ${RVM_GEMSET}"
-    exit
-else
-    rvm --create --install use ${RVM_GEMSET}
-fi
+[[ -f .ruby-version && -f .ruby-gemset ]] && RVM_GEMSET=$(cat .ruby-version .ruby-gemset | tr "\\n" "@" | sed 's/@$//')
+[[ -z "${RVM_GEMSET}" ]] && echo "ERROR: no valid ruby gemset found" && exit
+rvm --create --install use ${RVM_GEMSET}
 
 # prepare bundler
 export BUNDLE_GEMFILE=Gemfile
 which bundle | grep ${RVM_GEMSET} >/dev/null || gem install bundler --no-rdoc --no-ri
 bundle check >/dev/null || bundle install
 
-# run ruby build with parameters
+# run rake with parameters
 bundle exec rake -f Rakefile $@
